5e712b3d547a494a610d9f5b6587829cbcd03d2a
Gabriela Salas <gabrielasalas@gmail.com>
2015-10-30 12:12:30 -0600

    design updates

--- wwwroot/includes/footer.php
+++ wwwroot/includes/footer.php
@@ -1,14 +1,31 @@
 <?php
 echo "
-	<footer class='footer navbar navbar-default'>
-		<ul>
-			<li><div class='btn btn-lg btn-primary'  role='button'>PHYSICIANS ENTRY</div></li>
-			<li><div class='btn btn-lg btn-primary'  role='button'>PRIVACY POLICY</div></li>
-			<li><div class='btn btn-lg btn-primary'  role='button'>TERMS &reg; CONDITIONS</div></li>
-			<li class='social'><div class='btn btn-lg btn-primary btn-social'  role='button'><span class='facebook-icon'></span></div></li>
-			<li class='social'><div class='btn btn-lg btn-primary btn-social'  role='button'><span class='twitter-icon'></span></div></li>
-		</ul>
-		<div class='brand'>spacebander corporation 2015. all rights reserved</div>
-	</footer>
+		<div id='footer' class='dark-gray'>
+			<div class='container'>
+				<div class='row bottom_links'>
+					<div class='col-lg-10 col-lg-offset-1'>
+						<ul>
+							<li><a class='btn_footer' href='portal.php'>Physician Portal</a></li>
+							<li><a class='btn_footer' href='privacy.php'>Privacy Policy</a></li>
+							<li><a class='btn_footer' href='terms.php'>Terms of Use</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class='row bottom_links'>
+					<div class='col-lg-8 col-lg-offset-2'>
+						<ul>
+							<li><a class='social-facebook' href='www.facebook.com/spacebander'><img src='img/desktop/icon_facebook.jpg'></a></li>
+							<li><a class='social-twitter' href='www.twitter.com/spacebander'><img src='img/desktop/icon_twitter.jpg'></a></li>	
+						</ul>
+					</div>
+				</div>
+				<div class='row'>			
+					<div class='col-lg-10 col-lg-offset-1'> 
+						<p>USE OF THIS SITE SIGNIFIES YOUR AGREEMENT TO THE <a href='terms.php'>TERMS OF USE</a> AND TO OUR <a href='privacy.php'>PRIVACY POLICY.</a></p>
+						<p> &copy; SPACEBANDER CORPORATION 2015  •  ALL RIGHTS RESERVED </p>
+				    </div>
+			    </div> 
+			</div>
+		</div><!-- footer===================== -->
 ";
 ?>
