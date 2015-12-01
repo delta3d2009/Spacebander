5496eba2a535e2d638f8ad8f65e8dbd5904e4ef9
Marlon Ulate <marlonjuc@hotmail.com>
2015-12-01 15:51:23 -0600

    Adding DataBase - SQL / PL

---
+++ database/health-care-providers.sql
@@ -0,0 +1,304 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1187\cocoasubrtf390
+{\fonttbl\f0\fswiss\fcharset0 ArialMT;}
+{\colortbl;\red255\green255\blue255;}
+\margl1440\margr1440\vieww25440\viewh11400\viewkind0
+\deftab720
+\pard\pardeftab720
+
+\f0\fs36 \cf0 CREATE TABLE Physician ( \
+ID INT NOT NULL AUTO_INCREMENT, \
+Company VARCHAR(50) NOT NULL,\
+FirstName VARCHAR(80) NOT NULL, \
+LastName VARCHAR(80) NOT NULL, \
+Address VARCHAR(80) NOT NULL,\
+Suite VARCHAR(80) NOT NULL,\
+City VARCHAR(40) NOT NULL,\
+State VARCHAR(40) NOT NULL,\
+Zip INT NOT NULL,\
+Country VARCHAR(50) NOT NULL,\
+Phone VARCHAR(20) NOT NULL,\
+Cell VARCHAR(20) NOT NULL,\
+Fax VARCHAR(20) NOT NULL,\
+Email VARCHAR(30) NULL,\
+Office VARCHAR(50) NULL,\
+Lat FLOAT(20,16) NOT NULL,\
+Lng FLOAT(20,16) NOT NULL,\
+PRIMARY KEY(ID)\
+);\
+\
+CREATE TABLE Account(\
+ID INT NOT NULL AUTO_INCREMENT,\
+Username VARCHAR(16) NOT NULL,\
+Password VARCHAR(32) NOT NULL,\
+Email VARCHAR(30) NOT NULL,\
+Token INT NULL,\
+PRIMARY KEY(ID)\
+);\
+\
+CREATE TABLE Contact ( \
+ID INT NOT NULL AUTO_INCREMENT, \
+FirstName VARCHAR(80) NOT NULL, \
+LastName VARCHAR(80) NOT NULL, \
+Phone VARCHAR(20) NOT NULL,\
+Email VARCHAR(30) NULL,\
+Message VARCHAR(200) NULL,\
+Date DATE NOT NULL,\
+PRIMARY KEY(ID)\
+);\
+\
+CREATE TABLE RemovedPhysician LIKE Physician;\
+\
+DELIMITER ;;\
+CREATE PROCEDURE getPhysicians(IN paramLat FLOAT, IN paramLng FLOAT, IN paramRadius INT)\
+BEGIN\
+\pard\tx20800\pardeftab720
+\cf0 SELECT Company, FirstName, LastName, Address, Suite, City, State, Country, Phone, Cell, Fax, Zip, Email, Office, Lat, Lng, ( 3959 * acos( cos( radians(paramLat) ) * cos( radians( Lat ) ) * cos( radians( Lng ) - radians(paramLng) ) + sin( radians(paramLat) ) * sin( radians( Lat ) ) ) ) AS Distance FROM Physician HAVING Distance < paramRadius ORDER BY Distance LIMIT 0 , 20;\
+END;;\
+\
+\pard\pardeftab720
+\cf0 INSERT INTO Account (Username, Password, Email, Token) VALUES ('admin', MD5('123queso'), 'marlonjuc@hotmail.com', 0);\
+INSERT INTO Account (Username, Password, Email, Token) VALUES ('gaby', MD5('123queso'), 'gabrielasalas@gmail.com', 0);\
+\
+DELIMITER ;;\
+CREATE PROCEDURE getUserAccount(IN user VARCHAR(16), IN pass VARCHAR(32))\
+BEGIN\
+SELECT COUNT(Username) FROM Account WHERE Username = user AND Password = MD5(pass);\
+END;;\
+\
+DELIMITER ;;\
+CREATE PROCEDURE insertPhysician(IN paramCompany VARCHAR(50), IN paramFirstName VARCHAR(80), IN paramLastName VARCHAR(80), IN paramAddress VARCHAR(80), paramSuite VARCHAR(80), IN paramCity VARCHAR(40), IN paramState VARCHAR(40), IN paramZip INT, IN paramCountry VARCHAR(50), IN paramPhone VARCHAR(20), IN paramCell VARCHAR(20), IN paramFax VARCHAR(20), IN paramEmail VARCHAR(30), IN paramOffice VARCHAR(50), IN paramLat FLOAT, IN paramLng FLOAT )\
+BEGIN\
+DECLARE counter INT;\
+SET counter = 0;\
+SELECT COUNT(ID) INTO counter FROM Physician WHERE Address = paramAddress AND Suite = paramSuite;\
+IF (counter > 0)\
+THEN\
+SELECT counter;\
+ELSE\
+INSERT INTO Physician (Company, FirstName, LastName, Address, Suite, City, State, Zip, Country, Phone, Cell, Fax, Email, Office, Lat, Lng) VALUES (paramCompany, paramFirstName, paramLastName, paramAddress, paramSuite, paramCity, paramState, paramZip, paramCountry, paramPhone, paramCell, paramFax, paramEmail, paramOffice, paramLat, paramLng);\
+SELECT 0;\
+END IF;\
+END;;\
+\
+DELIMITER ;;\
+CREATE PROCEDURE insertContact(IN paramFirstName VARCHAR(80), IN paramLastName VARCHAR(80), IN paramPhone VARCHAR(20), IN paramEmail VARCHAR(30), IN paramMessage VARCHAR(200) )\
+BEGIN\
+INSERT INTO Contact (FirstName, LastName, Phone , Email, Message, Date) VALUES (paramFirstName, paramLastName, paramPhone, paramEmail, paramMessage, CURDATE());\
+SELECT 0;\
+END;;\
+\
+DELIMITER ;;\
+CREATE PROCEDURE searchPhysicians(IN paramColumn VARCHAR(80), IN paramStartRow INT)\
+BEGIN\
+IF paramColumn = "LastName"\
+THEN\
+SELECT * FROM Physician ORDER BY LastName ASC LIMIT paramStartRow, 10;\
+ELSEIF paramColumn = "Address"\
+THEN\
+SELECT * FROM Physician ORDER BY Country ASC, State ASC, City ASC, Address ASC LIMIT paramStartRow, 10;\
+ELSEIF paramColumn = "Zip"\
+THEN\
+SELECT * FROM Physician ORDER BY Zip, LastName ASC LIMIT paramStartRow, 10;\
+ELSE\
+SELECT * FROM Physician ORDER BY ID ASC LIMIT paramStartRow, 10;\
+END IF;\
+END;;\
+\
+DELIMITER ;;\
+CREATE PROCEDURE searchPhysiciansASC(IN paramColumn VARCHAR(80), IN paramStartRow INT)\
+BEGIN\
+IF paramColumn = "FirstName"\
+THEN\
+SELECT * FROM Physician ORDER BY FirstName ASC LIMIT paramStartRow, 10;\
+ELSEIF paramColumn = "LastName"\
+THEN\
+SELECT * FROM Physician ORDER BY LastName ASC LIMIT paramStartRow, 10;\
+ELSEIF paramColumn = "Company"\
+THEN\
+SELECT * FROM Physician ORDER BY Company ASC LIMIT paramStartRow, 10;\
+ELSEIF paramColumn = "Address"\
+THEN\
+SELECT * FROM Physician ORDER BY Address ASC LIMIT paramStartRow, 10;\
+ELSEIF paramColumn = "Suite"\
+THEN\
+SELECT * FROM Physician ORDER BY Suite ASC LIMIT paramStartRow, 10;\
+ELSEIF paramColumn = "Zip"\
+THEN\
+SELECT * FROM Physician ORDER BY Zip ASC LIMIT paramStartRow, 10;\
+ELSEIF paramColumn = "City"\
+THEN\
+SELECT * FROM Physician ORDER BY City ASC LIMIT paramStartRow, 10;\
+ELSEIF paramColumn = "State"\
+THEN\
+SELECT * FROM Physician ORDER BY State ASC LIMIT paramStartRow, 10;\
+ELSEIF paramColumn = "Country"\
+THEN\
+SELECT * FROM Physician ORDER BY Country ASC LIMIT paramStartRow, 10;\
+ELSEIF paramColumn = "Phone"\
+THEN\
+SELECT * FROM Physician ORDER BY Phone ASC LIMIT paramStartRow, 10;\
+ELSEIF paramColumn = "Cell"\
+THEN\
+SELECT * FROM Physician ORDER BY Cell ASC LIMIT paramStartRow, 10;\
+ELSEIF paramColumn = "Fax"\
+THEN\
+SELECT * FROM Physician ORDER BY Fax ASC LIMIT paramStartRow, 10;\
+ELSEIF paramColumn = "Country"\
+THEN\
+SELECT * FROM Physician ORDER BY Country ASC LIMIT paramStartRow, 10;\
+ELSEIF paramColumn = "Phone"\
+THEN\
+SELECT * FROM Physician ORDER BY Phone ASC LIMIT paramStartRow, 10;\
+ELSEIF paramColumn = "Email"\
+THEN\
+SELECT * FROM Physician ORDER BY Email ASC LIMIT paramStartRow, 10;\
+ELSEIF paramColumn = "Office"\
+THEN\
+SELECT * FROM Physician ORDER BY Office ASC LIMIT paramStartRow, 10;\
+ELSE\
+SELECT * FROM Physician ORDER BY ID ASC LIMIT paramStartRow, 10;\
+END IF;\
+END;;\
+\
+DELIMITER ;;\
+CREATE PROCEDURE searchPhysiciansDESC(IN paramColumn VARCHAR(80), IN paramStartRow INT)\
+BEGIN\
+IF paramColumn = "FirstName"\
+THEN\
+SELECT * FROM Physician ORDER BY FirstName DESC LIMIT paramStartRow, 10;\
+ELSEIF paramColumn = "LastName"\
+THEN\
+SELECT * FROM Physician ORDER BY LastName DESC LIMIT paramStartRow, 10;\
+ELSEIF paramColumn = "Company"\
+THEN\
+SELECT * FROM Physician ORDER BY Company DESC LIMIT paramStartRow, 10;\
+ELSEIF paramColumn = "Address"\
+THEN\
+SELECT * FROM Physician ORDER BY Address DESC LIMIT paramStartRow, 10;\
+ELSEIF paramColumn = "Suite"\
+THEN\
+SELECT * FROM Physician ORDER BY Suite DESC LIMIT paramStartRow, 10;\
+ELSEIF paramColumn = "Zip"\
+THEN\
+SELECT * FROM Physician ORDER BY Zip DESC LIMIT paramStartRow, 10;\
+ELSEIF paramColumn = "City"\
+THEN\
+SELECT * FROM Physician ORDER BY City DESC LIMIT paramStartRow, 10;\
+ELSEIF paramColumn = "State"\
+THEN\
+SELECT * FROM Physician ORDER BY State DESC LIMIT paramStartRow, 10;\
+ELSEIF paramColumn = "Country"\
+THEN\
+SELECT * FROM Physician ORDER BY Country DESC LIMIT paramStartRow, 10;\
+ELSEIF paramColumn = "Phone"\
+THEN\
+SELECT * FROM Physician ORDER BY Phone DESC LIMIT paramStartRow, 10;\
+ELSEIF paramColumn = "Cell"\
+THEN\
+SELECT * FROM Physician ORDER BY Cell DESC LIMIT paramStartRow, 10;\
+ELSEIF paramColumn = "Fax"\
+THEN\
+SELECT * FROM Physician ORDER BY Fax DESC LIMIT paramStartRow, 10;\
+ELSEIF paramColumn = "Country"\
+THEN\
+SELECT * FROM Physician ORDER BY Country DESC LIMIT paramStartRow, 10;\
+ELSEIF paramColumn = "Phone"\
+THEN\
+SELECT * FROM Physician ORDER BY Phone DESC LIMIT paramStartRow, 10;\
+ELSEIF paramColumn = "Email"\
+THEN\
+SELECT * FROM Physician ORDER BY Email DESC LIMIT paramStartRow, 10;\
+ELSEIF paramColumn = "Office"\
+THEN\
+SELECT * FROM Physician ORDER BY Office DESC LIMIT paramStartRow, 10;\
+ELSE\
+SELECT * FROM Physician ORDER BY ID DESC LIMIT paramStartRow, 10;\
+END IF;\
+END;;\
+\
+DELIMITER ;;\
+CREATE PROCEDURE getCountPhysicians()\
+BEGIN\
+SELECT COUNT(ID) FROM Physician;\
+END;;\
+\
+DELIMITER ;;\
+CREATE PROCEDURE getTableInfo(IN paramTableName VARCHAR(80))\
+BEGIN\
+IF paramTableName = "Physician"\
+THEN\
+SELECT * FROM Physician;\
+ELSEIF paramTableName = "Account"\
+THEN\
+SELECT * FROM Account;\
+ELSE\
+SELECT * FROM Physician;\
+END IF;\
+END;;\
+\
+DELIMITER ;;\
+CREATE PROCEDURE SearchPhysicianByID( IN paramID INT)\
+BEGIN\
+SELECT * FROM Physician WHERE ID = paramID;\
+END;;\
+\
+DELIMITER ;;\
+CREATE PROCEDURE removePhysicianByID( IN paramID INT)\
+BEGIN\
+IF EXISTS(SELECT ID FROM Physician WHERE ID = paramID)\
+THEN\
+INSERT RemovedPhysician SELECT * FROM Physician WHERE ID = paramID;\
+SELECT ID FROM Physician WHERE ID = paramID; \
+DELETE FROM Physician WHERE ID = paramID;\
+ELSE\
+SELECT 0;\
+END IF;\
+END;;\
+\
+DELIMITER ;;\
+CREATE PROCEDURE updatePhysician(IN paramID INT, IN paramCompany VARCHAR(50), IN paramFirstName VARCHAR(80), IN paramLastName VARCHAR(80), IN paramAddress VARCHAR(80), IN paramSuite VARCHAR(80), IN paramCity VARCHAR(40), IN paramState VARCHAR(40), IN paramZip INT, IN paramCountry VARCHAR(50), IN paramPhone VARCHAR(20), IN paramCell VARCHAR(20), IN paramFax VARCHAR(20), IN paramEmail VARCHAR(30), IN paramOffice VARCHAR(50) )\
+BEGIN\
+IF EXISTS(SELECT ID FROM Physician WHERE ID = paramID)\
+THEN\
+UPDATE Physician SET Company = paramCompany, FirstName = paramFirstName, LastName = paramLastName, Address = paramAddress, Suite = paramSuite, City = paramCity, State = paramState, Zip = paramZip, Country = paramCountry, Phone = paramPhone, Cell = paramCell, fax = paramFax, Email = paramEmail, Office = paramOffice WHERE ID = paramID;\
+SELECT ID FROM Physician WHERE ID = paramID;\
+ELSE\
+SELECT 0;\
+END IF;\
+END;;\
+\
+DELIMITER ;;\
+CREATE PROCEDURE getToken(IN paramEmail VARCHAR(30))\
+BEGIN\
+IF EXISTS (SELECT ID FROM Account WHERE Email = paramEmail)\
+THEN\
+UPDATE Account SET Token = FLOOR(RAND()*(9000-1000)+1000) WHERE Email = paramEmail;\
+SELECT Token, UserName FROM Account WHERE Email = paramEmail;\
+ELSE\
+SELECT 0;\
+END IF;\
+END;;\
+\
+DELIMITER ;;\
+CREATE PROCEDURE resetPassword(IN paramEmail VARCHAR(30), IN paramPassword VARCHAR(32), IN paramToken INT )\
+BEGIN\
+IF EXISTS(SELECT ID FROM Account WHERE Email = paramEmail AND Token = paramToken)\
+THEN\
+UPDATE Account SET Password = MD5(paramPassword) WHERE Email = paramEmail AND Token = paramToken;\
+SELECT ID FROM Account WHERE Email = paramEmail AND Token = paramToken;\
+ELSE\
+SELECT 0;\
+END IF;\
+END;;\
+\
+DELIMITER ;;\
+CREATE PROCEDURE filterPhysicians(IN paramFilter VARCHAR(80), IN paramStartRow INT)\
+BEGIN\
+SELECT * FROM Physician WHERE ID Like paramFilter OR Company LIKE paramFilter OR FirstName LIKE paramFilter OR LastName  LIKE paramFilter OR Address LIKE paramFilter OR Suite LIKE paramFilter OR City LIKE paramFilter OR State LIKE paramFilter OR Zip LIKE paramFilter OR Country LIKE paramFilter OR Phone LIKE paramFilter OR Cell LIKE paramFilter OR Fax LIKE paramFilter OR Email LIKE paramFilter OR Office LIKE paramFilter ORDER BY ID ASC LIMIT paramStartRow, 10;\
+END;;\
+\
+DELIMITER ;;\
+CREATE PROCEDURE getPhysiciansDataGrid()\
+BEGIN\
+SELECT DISTINCT p.ID, p.Company, p.FirstName, p.LastName, p.Address, p.Suite, p.City, p.State, p.Zip, p.Country, p.Phone, p.Cell, p.Fax, p.Email, p.Office FROM Physician p ORDER BY p.ID;\
+END;;}
---
+++ database/populate-fictitious-data.sql
@@ -0,0 +1,39 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1187\cocoasubrtf390
+{\fonttbl\f0\fswiss\fcharset0 ArialMT;}
+{\colortbl;\red255\green255\blue255;}
+\margl1440\margr1440\vieww25420\viewh13680\viewkind0
+\deftab720
+\pard\pardeftab720
+
+\f0\fs36 \cf0 INSERT INTO Physician (Company, FirstName, LastName, Address, Suite, City, State, Country, Phone, Cell, Fax, Zip, Lat, Lng, Email, Office) VALUES ('Dr. Frankie Johnnie & Luigo Too', 'Frankie','Roosvelt', '939 W El Camino Real', 'Apartment #2', 'Mountain View', 'California', 'United States', '+1 415-474-3030', '+1 415-474-3030', '+1 415-474-3030', 0,'37.386339', '-122.085823','','Secretary');\
+INSERT INTO Physician (Company, FirstName, LastName, Address, Suite, City, State, Country, Phone, Cell, Fax, Zip, Lat, Lng, Email, Office) VALUES ('Amici\\'s East Coast Medicine', 'Tony', 'Soprano','790 Castro St', 'Apartment #2', 'Mountain View', 'California', 'United States', '+1 415-474-3030', '+1 415-474-3030', '+1 415-474-3030', 0, '37.38714', '-122.083235','','Secretary');\
+INSERT INTO Physician (Company, FirstName, LastName, Address, Suite, City, State,Country, Phone, Cell, Fax, Zip, Lat, Lng, Email, Office) VALUES ('Kapp\\'s Drug Store', 'Martin', 'Heinze', '191 Castro St', 'Apartment #2', 'Mountain View', 'California', 'United States', '+1 415-474-3030', '+1 415-474-3030', '+1 415-474-3030', 0, '37.393885', '-122.078916','','Secretary');\
+INSERT INTO Physician (Company, FirstName, LastName, Address, Suite, City, State, Country, Phone, Cell, Fax, Zip, Lat, Lng, Email, Office) VALUES ('Billy Bob Drugs: Mountain View', 'William Bob','Thorton', '570 N Shoreline Blvd', 'Apartment #2', 'Mountain View', 'California', 'United States', '+1 415-474-3030', '+1 415-474-3030', '+1 415-474-3030', 0, '37.402653', '-122.079354','','Secretary');\
+INSERT INTO Physician (Company, FirstName, LastName, Address, Suite, City, State, Country, Phone, Cell, Fax, Zip, Lat, Lng, Email, Office) VALUES ('Dr. Jekyll and Mr Hide', 'Carl', 'Jekyll', '619 Escuela Ave', 'Apartment #2', 'Mountain View', 'California', 'United States', '+1 415-474-3030', '+1 415-474-3030', '+1 415-474-3030', 0, '37.394011','-122.095528','','Secretary');\
+INSERT INTO Physician (Company, FirstName, LastName, Address, Suite, City, State, Country, Phone, Cell, Fax, Zip, Lat, Lng, Email, Office) VALUES ('Dr. Fishel\\'s Health', 'Fitzgerald', 'Fishel', '4546 El Camino Real', 'Apartment #2', 'Los Altos', 'California', 'United States', '+1 415-474-3030', '+1 415-474-3030', '+1 415-474-3030', 0, '37.401724','-122.114646','','Secretary');\
+\
+INSERT INTO Physician (Company, FirstName, LastName, Address, Suite, City, State, Zip, Country, Phone, Cell, Fax, Office, Email, Lat, Lng) VALUES ('New Jersey Hospital Association', 'John', 'Smith', '60 Alexander Rd', 'Office #17', 'Princeton', 'New Jersey', 08540, 'United States', '+1 609-275-4000', '+1 609-275-4000', '+1 609-275-4000', 'Assistant', 'test@gmail.com', '40.330276', '-74.632348');\
+\
+INSERT INTO Physician (Company, FirstName, LastName, Address, Suite, City, State, Zip, Country, Phone, Cell, Fax, Office, Email, Lat, Lng) VALUES ('Morristown Medical Center', 'Roberto', 'Ayala', '100 Madison Ave', 'Office #02', 'Morristown', 'New Jersey', 07960, 'United States', '+1 973-971-5000', '+1 973-971-5000', '+1 973-971-5000','Assistant', 'test@gmail.com','40.789366', '-74.463980');\
+\
+INSERT INTO Physician (Company, FirstName, LastName, Address, Suite, City, State, Zip, Country, Phone, Cell, Fax, Office, Email, Lat, Lng) VALUES ('St. Clare\\'s Hospital', 'Alex', 'Ferguson', '400 W Blackwell St', 'Floor #3. Office #02', 'Dover', 'New Jersey', 07801, 'United States', '+1 564-971-5000', '+1 564-971-5000', '+1 564-971-5000','Assistant', 'test@gmail.com','40.884966', '-74.574690');\
+\
+INSERT INTO Physician (Company, FirstName, LastName, Address, Suite, City, State, Zip, Country, Phone, Cell, Fax, Office, Email, Lat, Lng) VALUES ('NewYork-Presbyterian/Weill Cornell Medical Center', 'Rachel', 'Green', '525 E 70th St', 'Office #16', 'New York', 'New York',10065, 'United States', '+1 212-746-5454', '+1 212-746-5454', '+1 212-746-5454', 'Assistant', 'test@gmail.com','40.764867', '-73.953985');\
+\
+INSERT INTO Physician (Company, FirstName, LastName, Address, Suite, City, State, Zip, Country, Phone, Cell, Fax, Office, Email, Lat, Lng) VALUES ('Mount Sinai Hospital', 'Jack', 'La Motta', '203 W 12th St', 'Office #23', 'New York', 'New York',10011, 'United States', '+1 212-746-5454', '+1 212-746-5454', '+1 212-746-5454', 'Assistant', 'test@gmail.com', '40.884966', '-74.574690');\
+\
+INSERT INTO Physician (Company, FirstName, LastName, Address, Suite, City, State, Zip, Country, Phone, Cell, Fax, Office, Email, Lat, Lng) VALUES ('Columbia University Medical Center', 'Isidor Isaac', 'Rabin', '1790 Broadway', 'Floor #3. Office #14', 'New York', 'New York',10019,'United States','+1 212-305-7114', '+1 212-305-7114', '+1 212-305-7114','Assistant', 'test@gmail.com','40.766847', '-73.981497');\
+\
+INSERT INTO Physician (Company, FirstName, LastName, Address, Suite, City, State, Zip, Country, Phone, Cell, Fax, Office, Email, Lat, Lng) VALUES ('Greater New York Hospital', 'Gianluigi', 'Buffon', '75 Ninth Ave', 'Floor #14. Office #32', 'New York', 'New York',10011,'United States','+1 646-638-2641', '+1 646-638-2641', '+1 646-638-2641', 'Assistant', 'test@gmail.com', '40.741996', '-74.004862');\
+\
+INSERT INTO Physician (Company, FirstName, LastName, Address, Suite, City, State, Zip, Country, Phone, Cell, Fax, Office, Email, Lat, Lng) VALUES ('Florida Hospital Orlando', 'Daffy', 'Duck', '601 E Rollins St', 'Floor #14. Office #32', 'Orlando', 'Florida',32803,'United States', '+1 407-303-5600', '+1 407-303-5600', '+1 407-303-5600', 'Assistant', 'test@gmail.com', '28.574426', '-81.369977');\
+\
+INSERT INTO Physician (Company, FirstName, LastName, Address, Suite, City, State, Zip, Country, Phone, Cell, Fax, Office, Email, Lat, Lng) VALUES ('Hospital Rafael \'c1ngel Calder\'f3n Guardia', 'Longino', 'Soto Pacheco', 'Calle 17 x Avenida 7 y 9', 'Consultorio A5', 'San Jos\'e9', 'San Jos\'e9',10105,'Costa Rica', '2212 1000', '2212 1000', '2212 1000', 'Assistant', 'test@gmail.com','9.936907', '-84.069431');\
+\
+INSERT INTO Physician (Company, FirstName, LastName, Address, Suite, City, State, Zip, Country, Phone, Cell, Fax, Office, Email, Lat, Lng) VALUES ('Hospital M\'e9xico', 'Juan Diego', 'Casafont', 'Transversal 74', 'Consultorio B3', 'San Jos\'e9', 'San Jos\'e9',10105,'Costa Rica', '2345 9090', '2345 9090', '2345 9090', 'Assistant', 'test@gmail.com', '9.951115', '-84.114794');\
+\
+INSERT INTO Physician (Company, FirstName, LastName, Address, Suite, City, State, Zip, Country, Phone, Cell, Fax, Office, Email, Lat, Lng) VALUES ('Hospital San Juan de Dios', 'Ileana', 'Ulate', 'Paseo Col\'f3n', 'Consultorio C4', 'San Jos\'e9', 'San Jos\'e9',10105,'Costa Rica', '2422 8686', '2422 8686', '2422 8686', 'Assistant', 'test@gmail.com', '9.933209', '-84.086444');\
+\
+INSERT INTO Physician (Company, FirstName, LastName, Address, Suite, City, State, Zip, Country, Phone, Cell, Fax, Office, Email, Lat, Lng) VALUES ('Hospital Carlos Luis Valverde Vega', 'Felix Angel', 'Salas', 'San Ram\'f3n', 'Consultorio D7', 'San Ram\'f3n', 'Alajuela',0,'Costa Rica', '2445 7011', '2445 7011', '2445 7011', 'Assistant', 'test@gmail.com', '10.092775', '-84.469928');\
+\
+}
